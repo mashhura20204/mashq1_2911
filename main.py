405735413db2d5f280c2b3a77023278fdc16c448
mashhura20204 <ibraimovamashhura0412@gmail.com>
2025-11-29 17:26:13 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+#1- misol
+kop  = lambda son:son * 5
+
+res  = kop(20)
+print(res)
+
+#2 - misol
+kop = lambda x,y: x * y
+res = kop(3, 5)
+print(res)
+
+
+#3 - misol
+juft_yoki_toq = lambda n: 'juft'if n % 2 == 0 else 'toq'
+print(juft_yoki_toq(8))
+print(juft_yoki_toq(5))
+
+#4 - misol
+
+katta = lambda x, y:x if x>y else y
+print(katta(10, 7))
+print(katta(3, 15))
